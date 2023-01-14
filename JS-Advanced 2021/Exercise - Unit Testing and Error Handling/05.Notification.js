2cdf031d0318136a365462edcddb00471da0c10c
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2023-01-14 17:43:14 +0000

    Unit Testing and Error Handling - Exercise

---
+++ JS-Advanced 2021/Exercise - Unit Testing and Error Handling/05.Notification.js
@@ -0,0 +1,9 @@
+function solve(message){
+    const resultDiv = document.querySelector('#notification');
+    resultDiv.style.display = 'block';
+    resultDiv.textContent = message;
+
+    resultDiv.addEventListener('click', ()=>{
+        resultDiv.style.display = 'none';
+    })
+}
